a88d8e2b69a2fcfc3f3fb28dbae0cef9877d900c
Francis Mangala <inestotofurume@FrancisM.local>
2021-06-13 16:39:41 -0400

    Analysis Complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,13 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 Suspension_Coil_table <- read.csv(file='Suspension_Coil.csv',check.names = F, stringsAsFactors = F)
 Total_Summary_table <- Suspension_Coil_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) 
 Lot_Summary_table <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+#Deliverable3
+t.test(Suspension_Coil_table$PSI,mu=1500)
+
+#T-Test PSI for each Manufacturing lot
+
+t.test(subset(Suspension_Coil_table,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+t.test(subset(Suspension_Coil_table,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+t.test(subset(Suspension_Coil_table,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+
